1c5cd8e24f31200f4e440b33c703fa7d72baa0fe
sspeirs <shanbicki@gmail.com>
2020-10-20 11:14:25 -0400

    Initial commit

---
+++ src/WeightyIssue.java
@@ -0,0 +1,13 @@
+/*sspeirs
+ * 10/20/2020, 11:07 AM */
+
+public class WeightyIssue {
+    public static void main(String args[]) {
+        //To turn in projects, you will use the VCS->Git->Push
+        /*Through your development process, you will commit your code,
+         which saves it to your account at GitHub, by using VCS->Git->Commit
+         Make sure you write a short comment and hit the Commit button.*/
+        /*write your algorithm here*/
+        
+    }
+}
